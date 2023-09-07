25c8ba87c17470bbe21c5bd136ce1dfc8c925e49
Milton Neto <milton33b@hotmail.com>
2023-09-07 17:09:54 -0300

    added aws fake credentials to run tests 2

--- app/src/test/java/com/br/hub/facecomparison/config/SpringAppTest.java
+++ app/src/test/java/com/br/hub/facecomparison/config/SpringAppTest.java
@@ -1,11 +1,21 @@
 package com.br.hub.facecomparison.config;
 
 import com.br.hub.facecomparison.config.config.SpringApp;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = SpringApp.class)
+@ActiveProfiles(value = "test")
 class SpringAppTest {
+
+	@BeforeAll
+	static void setupProperties() {
+		System.setProperty("aws.region", "us-east-1");
+		System.setProperty("aws.accessKeyId", "localstack");
+		System.setProperty("aws.secretAccessKey", "localstack");
+	}
 
 	@Test
 	void contextLoads() {
