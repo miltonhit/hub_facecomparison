a0fce7e947fa225ae64f637a29fa69d0afe2fb50
Milton Neto <milton33b@hotmail.com>
2023-09-08 10:33:28 -0300

    directory structure changed

--- src/main/java/com/br/hub/facecomparison/ComparisonController.java
+++ src/main/java/com/br/hub/facecomparison/ComparisonController.java
@@ -1,25 +1,24 @@
 package com.br.hub.facecomparison;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/facecomparison")
+@RequestMapping(path="/facecomparison", produces=MediaType.APPLICATION_JSON_VALUE)
 public class ComparisonController {
 
     @Autowired
     private ComparisonService service;
 
     @GetMapping("/status")
-    public String getStatus() {
-        return "RUNNING!";
+    public Status getStatus() {
+        return new Status("OK AND RUNNING");
     }
 
-    @PostMapping("/compare")
+    @PostMapping(path="/compare", consumes=MediaType.APPLICATION_JSON_VALUE)
     public ComparisonOutput compare(@RequestBody  ComparisonInput input) throws Exception {
-        System.out.println("SEI_LA");
-
         try {
             float result = service.compareTwoPhotos(input.photo1(), input.photo2());
             return new ComparisonOutput(result);
@@ -31,4 +30,5 @@ public class ComparisonController {
 
     record ComparisonInput(String photo1, String photo2) { }
     record ComparisonOutput(float similarity) { }
+    record Status(String status) { }
 }
--- src/main/java/com/br/hub/facecomparison/exception/InvalidImageException.java
+++ src/main/java/com/br/hub/facecomparison/exception/InvalidImageException.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.IOException;
 
-@ResponseStatus(value= HttpStatus.BAD_REQUEST, reason="image1 or image2 are not valid or do not have faces to compare.")
+@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="image1 or image2 are not valid or do not have faces to compare.")
 public class InvalidImageException extends IOException {
 }
